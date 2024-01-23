4ffaddb9f697198a811dc8877c377ac71ec24605
Yeakub1 <mryeakub10@gmail.com>
2024-01-23 21:20:14 +0600

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5cd183",
+        secondary: "#afd6e9",
+        accent: "#2d9f53",
+        bgColor: "#fefbfd",
+        textColor: "#1c0815",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
